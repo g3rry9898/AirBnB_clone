4b3d4fbd549bf373d0bbf9ed4bffeea00da0f4ad
g3rry9898 <hydrogenbond36@gmail.com>
2024-03-11 08:23:32 -0700

    l

--- console.py
+++ console.py
@@ -4,13 +4,12 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     
     def do_EOF(self, line):
+        """quiting the command prompt"""
+        return True
+    def do_quit(self, line):
+        """quiting the console"""
+        print("exiting prompt")
         return True
 
-    def do_quit(self, line):
-        print("\nexiting")
-
-
-    def do_empty_line(self, line):
-        pass
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
